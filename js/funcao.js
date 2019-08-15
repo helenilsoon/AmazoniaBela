b0a57920bc9f950277f0c0ba5a556e1a84e7d18a
helenilson <helenilsoon@gmail.com>
2019-08-15 17:21:34 -0400

    ajustes feito no java script

--- js/funcao.js
+++ js/funcao.js
@@ -5,19 +5,22 @@ function showSlides(){
     var i;
     var slides = document.getElementsByClassName('slide');
     var dots= document.getElementsByClassName('dot');
-    for( i = 0; i <  slides.length; 1++ ){
+    for( i = 0; i <  slides.length; i++ ){
         slides[i].style.display = "none";
     }
     slideIndex++;
+
     if(slideIndex > slides.length){
         slideIndex = 1;
-    }   
+        }   
     for( i = 0; i < dots.length; i++ ){
-        dots[i].className = dots[i].className.replace(" active","");
+        dots[i].className = dots[i].className.replace(" ativo","");
         
-    }
-    slides[slideIndex-1].style.display ="block";
-    dots[slideIndex-1].className +=" active";
-    setTimeout(showSlides,2000);
+        }
 
+    slides[slideIndex-1].style.display ="block";
+    dots[slideIndex-1].className +=" ativo";
+    setTimeout(showSlides,5000);
+    
 }
+
